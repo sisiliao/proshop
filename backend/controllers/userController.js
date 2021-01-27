fccea6703f391fd9fc0ca88c8cc414caca815f1c
sss1217 <siyun.liao@mail.mcgill.ca>
2021-01-27 17:13:18 -0500

    Frontend - finished user authentication and user profile screen, forms etc

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,7 +69,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
     res.json({
       _id: user._id,
       name: user.name,
-      email: user.name,
+      email: user.email,
       isAdmin: user.isAdmin,
     })
   } else {
@@ -78,4 +78,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser }
+//@desc     Update user profile
+//@route    PUT /api/users/profile
+//@access   Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  let user = await User.findById(req.user._id)
+  const { change } = req.body
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('user not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile }
