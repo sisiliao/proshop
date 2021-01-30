a500912ad25a7695a24b0a32c5f18fc8f67ba438
sss1217 <siyun.liao@mail.mcgill.ca>
2021-01-29 23:12:38 -0500

    Finished Checkout process. listing orders on ProfileScreen, OrderDetails Screen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
       <Header></Header>
       <main className='py-3'>
         <Container>
+          <Route path='/order/:id' component={OrderScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/payment' component={PaymentScreen} />
