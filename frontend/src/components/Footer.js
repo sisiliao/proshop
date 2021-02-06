37dbbdfc8908ed36a23e256811f8b236afa6477a
sss1217 <siyun.liao@mail.mcgill.ca>
2021-02-06 18:22:00 -0500

    minor fix

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className="text-center py-3">
-                        Copyright &copy; ProShop        
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; Secret Garden Florist
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
